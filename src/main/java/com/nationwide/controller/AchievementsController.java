190fa650a0b378d7f82b96e5d0dbb49d21fb278f
Lexi <alexiahh@hotmail.co.uk>
2019-12-04 10:27:29 +0000

    get achievements added

--- src/main/java/com/nationwide/controller/AchievementsController.java
+++ src/main/java/com/nationwide/controller/AchievementsController.java
@@ -1,9 +1,13 @@
 package com.nationwide.controller;
 
+import java.util.ArrayList;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.nationwide.data.Achievements;
 import com.nationwide.service.AchievementsService;
 
 
@@ -13,5 +17,10 @@ public class AchievementsController {
 	
 	@Autowired
 	private AchievementsService service;
+	
+	@GetMapping("/achievements/all")
+	public ArrayList<Achievements> getAllAchievements(){
+		return service.getAllAchievements();
+	}
 
 }
